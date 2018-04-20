5cb2824345ef69c4bb5980b328952fcf6ee2048a
Alex Scarlett <alex.scarlett91@gmail.com>
2018-04-20 17:09:48 -0400

    Added more starting dummy data for a larger variety of friend matches

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,19 +1,50 @@
 var friendsArray = [
   {
     name: 'Bob',
-    photo: 'https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX2578170.jpg',
+    photo: 'https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584_1280.png',
     answers: [2, 4, 3, 1, 1, 3, 5, 4, 2, 3]
   },
   {
-    name: 'Ahmed',
-    photo: 'https://images.megapixl.com/4707/47075253.jpg',
+    name: 'Charlie',
+    photo:
+      'https://i.pinimg.com/originals/97/f1/1e/97f11ea8bf2b7ec3545420e537af0e88.png',
     answers: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
   },
   {
     name: 'Alice',
     photo:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRb9I9QPV7Js2-nlb7h0Gfnq4umjIqWZCGGGWxyL7TinCYMyg1o',
+      'https://cf.ltkcdn.net/socialnetworking/images/orig/168796-350x350-girl-swear-icon.png',
+    answers: [3, 3, 1, 2, 5, 5, 4, 4, 2, 1]
+  },
+  {
+    name: 'John',
+    photo:
+      'https://cdn2.iconfinder.com/data/icons/avatar-2/512/smith_man-512.png',
     answers: [1, 2, 5, 3, 2, 5, 4, 1, 3, 5]
+  },
+  {
+    name: 'Samantha',
+    photo:
+      'https://cdn1.iconfinder.com/data/icons/user-pictures/100/female1-512.png',
+    answers: [4, 1, 3, 5, 3, 4, 4, 2, 5, 5]
+  },
+  {
+    name: 'Lauren',
+    photo:
+      'https://www.smmcare.com/wp-content/uploads/2017/12/team-members-avatar.jpg',
+    answers: [2, 1, 1, 5, 3, 5, 3, 4, 2, 1]
+  },
+  {
+    name: 'Jason',
+    photo:
+      'https://cdn.pixabay.com/photo/2017/12/18/03/01/black-avatar-3025348_1280.png',
+    answers: [5, 5, 1, 1, 3, 3, 4, 2, 4, 2]
+  },
+  {
+    name: 'Stephanie',
+    photo:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-wGJ_LERLb8h4GY8lIUA_sy6CEvYGII3PAYWTwUVV465Y9YSy',
+    answers: [3, 3, 3, 5, 1, 5, 1, 4, 4, 2]
   }
 ];
 
